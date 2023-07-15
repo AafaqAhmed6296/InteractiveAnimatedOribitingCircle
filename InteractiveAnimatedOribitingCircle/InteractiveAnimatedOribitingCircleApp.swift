da988231f09e9dee54b225a853646a8807c849b2
AafaqAhmed6296 <afaqahmad6296@gmail.com>
2023-07-15 13:22:24 +0500

    Initial Commit

---
+++ InteractiveAnimatedOribitingCircle/InteractiveAnimatedOribitingCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveAnimatedOribitingCircleApp.swift
+//  InteractiveAnimatedOribitingCircle
+//
+//  Created by Aafaq Ahmed Saqi on 15/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveAnimatedOribitingCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
